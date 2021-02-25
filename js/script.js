16da8e06b26705025694856f0913a9300b7ff81e
damasq <78789637+damasq@users.noreply.github.com>
2021-02-25 21:08:19 +0300

    test
    test

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function sub(btnId, menuId) {
+  const button = document.getElementById(btnId);
+  button.addEventListener('click', function() {
+    const target = document.getElementById(menuId);
+    target.classList.toggle('hidden')
+  })
+}
